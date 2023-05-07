6926e822d07b0b12eb0c513bb4ed083b1ebc1447
03li <moh.cvcv99@gmail.com>
2023-05-07 03:12:37 +0100

    add logging

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GetDataService } from './get-data.service';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-root',
@@ -7,33 +8,114 @@ import { GetDataService } from './get-data.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent{
+  logged:boolean = false
   val!:string;
   bcuid!:string;
   port!:string;
   wait = false;
+  alowed = false
+  password:string = ""
 
   constructor(private service:GetDataService) {}
 
-  onSearch() {
-    this.bcuid = ""
-    this.port = ""
-    this.service.getData().subscribe((data:any[])=>{
-      for (let i = 0; i < data.length; i++) {
-        if(data[i].dev==this.val) {
-          this.bcuid = data[i].bcuid
-          this.port = data[i].port
-          break
-        }
-      }
-
-      if(this.bcuid==="") {
-        this.wait = true
-      }else {
-        this.wait = false
-      }
-    })
+  decrypte(text: string) {
+    const textString = String(text);
+    return CryptoJS.AES.decrypt(textString.trim(), this.password.trim()).toString(CryptoJS.enc.Utf8);
   }
+  
+  onSearch() {
+      this.bcuid = ""
+      this.port = ""
+      let notFound = true;
+
+      this.service.nbOFfile().subscribe(nb=>{
+        for(let j = 0; j < nb ; j++) {
+          if(notFound) {
+            this.service.getData('db'+(j+1)+'.json').subscribe((data:any[])=>{
+                for (let i = 0; i < data.length; i++) {
+                  if(data[i].dev==this.val) {
+                    this.bcuid = this.decrypte(data[i].bcuid)
+                    this.port = this.decrypte(data[i].port)
+                    notFound = false;
+                    break
+                  }
+                }
+                if(this.bcuid==="") {
+                  this.wait = true
+                }else {
+                  this.wait = false
+                }
+            })
+          }
+        }
+      })
+  }
+  
+onLogin() {
+  this.service.getPass().subscribe(pass=>{
+    if(this.decrypte(pass)===this.password){
+      this.logged = true
+      this.alowed = false
+    }else{
+      this.logged = false
+      this.alowed = true
+    }
+  })
+}
 
 
 
+///////////////////////////////////////////////////////
+
+// encrypt(text: string) {
+//   const textString = String(text);
+//   return CryptoJS.AES.encrypt(textString.trim(), this.password.trim()).toString();
+// }
+
+// modifiedContent: string = '';
+// counter: number = 0;
+// entriesPerDownload: number = 20000;
+
+// onFileSelected(event: any) {
+//   const file: File = event.target.files[0];
+//   const reader: FileReader = new FileReader();
+//   reader.readAsText(file);
+
+//   reader.onload = () => {
+//     const fileContent: string = reader.result as string;
+//     this.modifiedContent = this.modifyFileContent(fileContent);
+//   };
+// }
+
+// modifyFileContent(fileContent: string): string {
+//   let modifiedContent: string = '';
+//   const entries: any[] = JSON.parse(fileContent);
+
+//   for (const entry of entries) {
+//     modifiedContent += `{ "bcuid": "${this.encrypt(entry.bcuid)}", "dev": ${entry.dev}, "port": "${this.encrypt(entry.port)}" },\n`;
+//     this.counter++;
+
+//     if (this.counter === this.entriesPerDownload) {
+//       this.downloadModifiedFile(modifiedContent);
+//       modifiedContent = ''; // Reset the modified content
+//       this.counter = 0; // Reset the counter
+//     }
+//   }
+
+//   modifiedContent = modifiedContent.trim();
+
+//   this.downloadModifiedFile(modifiedContent);
+//   return modifiedContent;
+// }
+
+// downloadModifiedFile(content: string) {
+//   const element = document.createElement('a');
+//   const file = new Blob([content], { type: 'application/json' });
+//   element.href = URL.createObjectURL(file);
+//   element.download = 'modified_file.json';
+//   document.body.appendChild(element);
+//   element.click();
+//   document.body.removeChild(element);
+// }
+//////////////////////////////////////////////////////////
 }
--- src/app/get-data.service.ts
+++ src/app/get-data.service.ts
@@ -6,12 +6,20 @@ import { Observable, filter, map, tap } from 'rxjs';
   providedIn: 'root'
 })
 export class GetDataService {
-  private url = 'https://raw.githubusercontent.com/03li/mockjson/main/db.json';
+  private url = 'https://raw.githubusercontent.com/03li/mockjson/main/';
 
   constructor(private http: HttpClient) { }
 
-  getData(): Observable<any[]> {
-    return this.http.get<any[]>(this.url);
+  nbOFfile():Observable<number> {
+    return this.http.get<number>('https://raw.githubusercontent.com/03li/mockjson/main/Nb')
+  }
+
+  getData(file:string): Observable<any[]> {
+    return this.http.get<any[]>(this.url+file);
+  }
+
+  getPass():Observable<string> {
+    return this.http.get<string>('https://raw.githubusercontent.com/03li/mockjson/main/pass')
   }
   
 }
